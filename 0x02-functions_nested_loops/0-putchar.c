2b49594bd67d3b0fc7020dfc2c2cbcd5514d3220
Ogbu Ezekiel Ike <zico@zico.local>
2022-03-15 23:10:14 +0100

    eed

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,10 +6,14 @@
  */
 int main(void)
 {
-	_putchar('M');
+	_putchar('-');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
 	_putchar('a');
-	_putchar('i');
-	_putchar('n');
+	_putchar('r');
 	_putchar('\n');
 
 	return (0);
