f98c421d3cf594e2c471c54f9cd54a484246531a
Ogbu Ezekiel Ike <zico@zico.local>
2022-03-15 22:51:11 +0100

    test

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include "zico.h"
+
+/**
+ * main -  prints zico, followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	_putchar('z');
+	_putchar('i');
+	_putchar('c');
+	_putchar('o');
+
+	return (0);
+}
