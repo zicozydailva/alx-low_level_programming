221b49debc7db74aa20d2caed6070c6d191b0bd1
Ogbu Ezekiel Ike <zico@zico.local>
2022-03-16 16:36:15 +0100

    three

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -7,12 +7,13 @@
 */
 
 int _isalpha(int c) {
-    if (c >= 1 && c <= 26) 
-    {
-        return (1);
-    } else if (c >= 5 && c <= 30) 
-    {
-        return (1);
-    }
-    return (0);
+    if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	return (0);
 }
