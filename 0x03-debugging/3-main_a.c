c242655ab8936f1ad77e8e82c77bc3c5186bfb84
Ogbu Ezekiel Ike <zico@zico.local>
2022-03-17 23:40:14 +0100

    wwss

--- 0x03-debugging/3-main_a.c
+++ 0x03-debugging/3-main_a.c
@@ -12,11 +12,13 @@ int main(void)
     int month;
 
     int day;
-    
+
     int year;
 
     month = 4;
+
     day = 01;
+    
     year = 1997;
 
     printf("Date: %02d/%02d/%04d\n", month, day, year);
--- 0x03-debugging/3-main_b.c
+++ 0x03-debugging/3-main_b.c
@@ -12,11 +12,13 @@ int main(void)
     int month;
 
     int day;
-    
+
     int year;
 
     month = 10;
+
     day = 4;
+    
     year = 1997;
 
     printf("Date: %02d/%02d/%04d\n", month, day, year);
